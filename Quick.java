ea7d5c36473113ca55ddbff713d5d1a0204fe514
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-03-14 07:57:35 -0400

    Create Quick.java

---
+++ Quick.java
@@ -0,0 +1,15 @@
+public class Quick {
+  
+  public static int quickSelect(int[] data, int k, int start, int end){
+    return -1;
+  }
+  
+  public static void quickSort(int[] data, int start, int end) {
+    
+  }
+  
+  public static int partition( int [] data, int start, int end){
+    return -1;
+  }
+
+}

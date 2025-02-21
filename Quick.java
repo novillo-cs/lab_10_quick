9a1b2dd3db7e66829ee6df6d04a469c1490b98f2
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2025-02-21 10:27:37 -0500

    Update Quick.java

--- Quick.java
+++ Quick.java
@@ -1,15 +1,25 @@
 public class Quick {
+
+
+  public static int partition( int [] data, int start, int end){
+    // You must declare variables to track left and right indices
+    return -1;
+  }
   
+  /* QuickSelect method to find the Kth smallest element
+  *  pivot  = partition(data, start, end)
+  *  if pivot > k -1
+	*    Recursive call to your QuickSelect - parameters: start=?, end=?
+  *  else if pivot < k -1
+	*    Recursive call to your QuickSelect - parameters: start=?, end=?
+  *  else
+	*    Kth element found return  the value [k-1]
+  */
   public static int quickSelect(int[] data, int k, int start, int end){
     return -1;
   }
   
-  public static void quickSort(int[] data, int start, int end) {
-    
-  }
   
-  public static int partition( int [] data, int start, int end){
-    return -1;
-  }
+
 
 }
